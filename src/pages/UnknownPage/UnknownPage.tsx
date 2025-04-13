a1c3790d4eb9f94ecfe9980f1a7e9d30ad1edd3d
Iwantschenko <160021860+Iwantschenko@users.noreply.github.com>
2025-04-13 12:22:30 +0300

    change UnknownPage

--- src/pages/UnknownPage/UnknownPage.tsx
+++ src/pages/UnknownPage/UnknownPage.tsx
@@ -1,15 +1,4 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
 export const UnknowPage = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setTimeout(() => {
-      navigate('..');
-    }, 1000);
-  });
-
   return (
     <div className="container">
       <h1 className="title">Page not found</h1>
